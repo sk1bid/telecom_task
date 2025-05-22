3d80eb929806575ca7ac8c105ae60b080876fd66
is342s12 <ivanov.artem.bld@gmail.com>
2025-05-22 16:03:32 +0700

    Part 1: python script

---
+++ checker.py
@@ -0,0 +1,29 @@
+import requests
+
+target_urls= [
+    "https://httpstat.us/101",
+    "https://httpstat.us/201",
+    "https://httpstat.us/301",
+    "https://httpstat.us/404",
+    "https://httpstat.us/500",    
+]
+
+def make_request(target_url: str):
+    response = requests.get(target_url)
+
+    if 100<=response.status_code<=300:
+        print(f"OK: {response.status_code}")
+        print(f"response body: {response.text}")
+    elif 400<=response.status_code<=500:
+        raise requests.exceptions.HTTPError(response=response)
+    else:
+        print(f"Unrecognised status code {response.status_code} for {url}")
+
+if __name__ == "__main__":
+    print("Starting requests to httpstat.us")
+    for i, url in enumerate(target_urls):
+        print(f"-----------REQUEST {i+1} to {url}---------------")
+        try:
+            make_request(url)
+        except requests.exceptions.HTTPError as e:
+            print(f"HTTP ERROR")
